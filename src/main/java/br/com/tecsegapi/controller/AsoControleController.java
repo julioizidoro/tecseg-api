fd70bcd65e750118b30ad586a39bb609219abb26
Julio Izidoro <jizidoro@globo.com>
2019-03-19 16:01:48 -0300

    consultas asocontrole

--- src/main/java/br/com/tecsegapi/controller/AsoControleController.java
+++ src/main/java/br/com/tecsegapi/controller/AsoControleController.java
@@ -44,24 +44,170 @@ public class AsoControleController {
 	@Autowired
 	private AsoTipoRepository asoTipoRepository;
 	
-	@GetMapping("/datavencimento/{datavencimentostart}/{datavencimentoend}")
-	public ResponseEntity<Optional<List<Asocontrole>>> listar(@PathVariable("datavencimentostart") String datavencimentostart, @PathVariable("datavencimentoend") String datavencimentoend) {
+	@GetMapping("/datavencimento/{datavencimentostart}/{datavencimentoend}/{nome}")
+	@Cacheable("consultaAsoControle")
+	public ResponseEntity<Optional<List<Asocontrole>>> findAllDataVencimento(@PathVariable("datavencimentostart") String datavencimentostart, 
+			@PathVariable("datavencimentoend") String datavencimentoend, @PathVariable("nome") String nome) {
 		Conversor c = new Conversor();
 		Date dataStart = c.ConvercaoStringData(datavencimentostart);
 		Date dataEnd = c.ConvercaoStringData(datavencimentoend);
-		Optional<List<Asocontrole>> asoControle = asoControleRepository.findAll(dataStart, dataEnd);
+		Optional<List<Asocontrole>> asoControle = asoControleRepository.findAllDataVencimento(dataStart, dataEnd, nome);
 		if (asoControle==null) {
 			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(asoControle);
 	}
 	
+	@GetMapping("/datavencimento/{datavencimentostart}/{datavencimentoend}/{nome}/{idfuncao}")
+	@Cacheable("consultaAsoControle")
+	public ResponseEntity<Optional<List<Asocontrole>>> finAllDataVencimentoFuncao(@PathVariable("datavencimentostart") String datavencimentostart, 
+			@PathVariable("datavencimentoend") String datavencimentoend, @PathVariable("nome") String nome, @PathVariable("idfuncao") int idfuncao) {
+		Conversor c = new Conversor();
+		Date dataStart = c.ConvercaoStringData(datavencimentostart);
+		Date dataEnd = c.ConvercaoStringData(datavencimentoend);
+		Optional<List<Asocontrole>> asoControle = asoControleRepository.findAllDataVencimentoFuncao(dataStart, dataEnd, nome, idfuncao);
+		if (asoControle==null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(asoControle);
+	}
+	
+	@GetMapping("/datavencimento/{datavencimentostart}/{datavencimentoend}/{nome}/{idloja}")
+	@Cacheable("consultaAsoControle")
+	public ResponseEntity<Optional<List<Asocontrole>>> finAllDataVencimentoLoja(@PathVariable("datavencimentostart") String datavencimentostart, 
+			@PathVariable("datavencimentoend") String datavencimentoend, @PathVariable("nome") String nome, @PathVariable("idloja") int idloja) {
+		Conversor c = new Conversor();
+		Date dataStart = c.ConvercaoStringData(datavencimentostart);
+		Date dataEnd = c.ConvercaoStringData(datavencimentoend);
+		Optional<List<Asocontrole>> asoControle = asoControleRepository.findAllDataVencimentoLoja(dataStart, dataEnd, nome, idloja);
+		if (asoControle==null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(asoControle);
+	}
+	
+	@GetMapping("/datavencimento/{datavencimentostart}/{datavencimentoend}/{nome}/{idasotipo}")
+	@Cacheable("consultaAsoControle")
+	public ResponseEntity<Optional<List<Asocontrole>>> finAllDataVencimentoTipo(@PathVariable("datavencimentostart") String datavencimentostart, 
+			@PathVariable("datavencimentoend") String datavencimentoend, @PathVariable("nome") String nome, @PathVariable("idasotipo") int idasotipo) {
+		Conversor c = new Conversor();
+		Date dataStart = c.ConvercaoStringData(datavencimentostart);
+		Date dataEnd = c.ConvercaoStringData(datavencimentoend);
+		Optional<List<Asocontrole>> asoControle = asoControleRepository.findAllDataVencimentoTipo(dataStart, dataEnd, nome, idasotipo);
+		if (asoControle==null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(asoControle);
+	}
+	
+	@GetMapping("/funcaoTipo/{idfuncao}/{idtipo}/{nome}")
+	@Cacheable("consultaAsoControle")
+	public ResponseEntity<Optional<List<Asocontrole>>> finAllFuncaoTipo(@PathVariable("idfuncao") int idfuncao, 
+			@PathVariable("idtipo") int idtipo, @PathVariable("nome") String nome) {
+		Optional<List<Asocontrole>> asoControle = asoControleRepository.findAllFuncaoTipo(idfuncao, nome, idtipo);
+		if (asoControle==null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(asoControle);
+	}
+	
+	@GetMapping("/funcaoloja/{idfuncao}/{idloja}/{nome}")
+	@Cacheable("consultaAsoControle")
+	public ResponseEntity<Optional<List<Asocontrole>>> finAllFuncaoLoja(@PathVariable("idfuncao") int idfuncao, 
+			@PathVariable("idtipo") int idtipo, @PathVariable("nome") String nome) {
+		Optional<List<Asocontrole>> asoControle = asoControleRepository.findAllFuncaoTipo(idfuncao, nome, idtipo);
+		if (asoControle==null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(asoControle);
+	}
+	
+	@GetMapping("/lojatipo/{idloja}/{idtipo}/{nome}")
+	@Cacheable("consultaAsoControle")
+	public ResponseEntity<Optional<List<Asocontrole>>> finAllLojaTipo(@PathVariable("idloja") int idloja, 
+			@PathVariable("idtipo") int idtipo, @PathVariable("nome") String nome) {
+		Optional<List<Asocontrole>> asoControle = asoControleRepository.findAllLojaTipo(idtipo, nome, idloja);
+		if (asoControle==null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(asoControle);
+	}
+	
+	@GetMapping("/loja/{idloja}/{nome}")
+	@Cacheable("consultaAsoControle")
+	public ResponseEntity<Optional<List<Asocontrole>>> finAllLoja(@PathVariable("idloja") int idloja, 
+			 @PathVariable("nome") String nome) {
+		Optional<List<Asocontrole>> asoControle = asoControleRepository.findAllLoja(idloja, nome);
+		if (asoControle==null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(asoControle);
+	}
+		
+	@GetMapping("/funcao/{idfuncao}/{nome}")
+	@Cacheable("consultaAsoControle")
+	public ResponseEntity<Optional<List<Asocontrole>>> finAllFuncao(@PathVariable("idfuncao") int idfuncao, 
+			@PathVariable("nome") String nome) {
+		Optional<List<Asocontrole>> asoControle = asoControleRepository.findAllFuncao(idfuncao, nome);
+		if (asoControle==null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(asoControle);
+	}
+	
+	@GetMapping("/tipo/{idtipo}/{nome}")
+	@Cacheable("consultaAsoControle")
+	public ResponseEntity<Optional<List<Asocontrole>>> finAllTipo(@PathVariable("idtipo") int idtipo, 
+			@PathVariable("nome") String nome) {
+		Optional<List<Asocontrole>> asoControle = asoControleRepository.findAllTipo(idtipo, nome);
+		if (asoControle==null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(asoControle);
+	}
+	
+	
+	@GetMapping("/nome/{nome}")
+	@Cacheable("consultaAsoControle")
+	public ResponseEntity<Optional<List<Asocontrole>>> finAllNome(@PathVariable("nome") String nome) {
+		Optional<List<Asocontrole>> asoControle = asoControleRepository.findAllNome(nome);
+		if (asoControle==null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(asoControle);
+	}
+	
+	@GetMapping("/datavencimento/{datavencimentostart}/{datavencimentoend}/{nome}/{idloja}/{idfuncao}/{idtipo}")
+	@Cacheable("consultaAsoControle")
+	public ResponseEntity<Optional<List<Asocontrole>>> finAll(@PathVariable("datavencimentostart") String datavencimentostart, 
+			@PathVariable("datavencimentoend") String datavencimentoend, @PathVariable("nome") String nome, 
+			@PathVariable("idloja") int idloja, @PathVariable("idfuncao") int idfuncao, @PathVariable("idtipo") int idtipo) {
+		Conversor c = new Conversor();
+		Date dataStart = c.ConvercaoStringData(datavencimentostart);
+		Date dataEnd = c.ConvercaoStringData(datavencimentoend);
+		Optional<List<Asocontrole>> asoControle = asoControleRepository.findAll(dataStart, dataEnd, nome, idloja, idfuncao, idtipo);
+		if (asoControle==null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(asoControle);
+	}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	@GetMapping
 	@Cacheable("consultaAsoControle")
 	public ResponseEntity<Optional<List<Asocontrole>>> listar() {
 		Conversor c = new Conversor();
 		Date data = c.SomarDiasData(new Date(), 60);
-		Optional<List<Asocontrole>> asoControle = asoControleRepository.findAllFinalizadoo(data);
+		Optional<List<Asocontrole>> asoControle = asoControleRepository.findAll(data);
 		if (asoControle==null) {
 			return ResponseEntity.notFound().build();
 		}
--- src/main/java/br/com/tecsegapi/repository/AsoControleRepository.java
+++ src/main/java/br/com/tecsegapi/repository/AsoControleRepository.java
@@ -13,16 +13,113 @@ import br.com.tecsegapi.model.Asocontrole;
 
 public interface AsoControleRepository extends JpaRepository<Asocontrole, Integer>{
 	
-	@Query("select a from Asocontrole a where a.datavencimento>= :dataVencimentoStar and a.datavencimento<= :dataVencimentoEnd "
-			+ " and a.finalizado=0 order by a.datavencimento")
-	Optional<List<Asocontrole>> findAll(
-	@Param("dataVencimentoStar") Date dataVencimentoStart,
-	@Param("dataVencimentoEnd") Date dataVencimentoEnd);
+	
 	
 	Optional<Asocontrole> findById(int Id);
 	
 	@Query("select a from Asocontrole a where a.finalizado=0 and  a.datavencimento<= :data order by a.datavencimento")
-	Optional<List<Asocontrole>> findAllFinalizadoo(
+	Optional<List<Asocontrole>> findAll(
 	@Param("data") Date data);
+	
+	//Somente Data Vencimento
+	@Query("select a from Asocontrole a where a.datavencimento>= :dataVencimentoStar and a.datavencimento<= :dataVencimentoEnd "
+			+ " and a.finalizado=0 and a.funcionario.nome like CONCAT('%', :nome, '%') order by a.datavencimento")
+	Optional<List<Asocontrole>> findAllDataVencimento(
+	@Param("dataVencimentoStar") Date dataVencimentoStart,
+	@Param("dataVencimentoEnd") Date dataVencimentoEnd,
+	@Param("nome") String nome);
+	
+	//Data Vencimento e funcao
+		@Query("select a from Asocontrole a where a.datavencimento>= :dataVencimentoStar and a.datavencimento<= :dataVencimentoEnd "
+				+ " and a.finalizado=0 and a.funcionario.funcao.idfuncao= :idfuncao and a.funcionario.nome like CONCAT('%', :nome, '%') order by a.datavencimento")
+		Optional<List<Asocontrole>> findAllDataVencimentoFuncao(
+		@Param("dataVencimentoStar") Date dataVencimentoStart,
+		@Param("dataVencimentoEnd") Date dataVencimentoEnd,
+		@Param("nome") String nome,
+		@Param("idfuncao") int idfuncao);
+		
+		//Data Vencimento e loja
+		@Query("select a from Asocontrole a where a.datavencimento>= :dataVencimentoStar and a.datavencimento<= :dataVencimentoEnd "
+				+ " and a.finalizado=0 and a.funcionario.loja.idloja= :idloja and a.funcionario.nome like CONCAT('%', :nome, '%') order by a.datavencimento")
+		Optional<List<Asocontrole>> findAllDataVencimentoLoja(
+		@Param("dataVencimentoStar") Date dataVencimentoStart,
+		@Param("dataVencimentoEnd") Date dataVencimentoEnd,
+		@Param("nome") String nome,
+		@Param("idloja") int idloja);
+		
+		//Data Vencimento e Tipo
+		@Query("select a from Asocontrole a where a.datavencimento>= :dataVencimentoStar and a.datavencimento<= :dataVencimentoEnd "
+				+ " and a.finalizado=0 and a.asotipo.idasotipo= :idasotipo and a.funcionario.nome like CONCAT('%', :nome, '%') order by a.datavencimento")
+		Optional<List<Asocontrole>> findAllDataVencimentoTipo(
+		@Param("dataVencimentoStar") Date dataVencimentoStart,
+		@Param("dataVencimentoEnd") Date dataVencimentoEnd,
+		@Param("nome") String nome,
+		@Param("idasotipo") int idtipo);
+
+		//Funcao e Tipo
+		@Query("select a from Asocontrole a where a.funcionario.funcao.idfuncao= :idfuncao " 
+				+ " and a.finalizado=0 and a.asotipo.idasotipo= :idasotipo and a.funcionario.nome like CONCAT('%', :nome, '%') order by a.datavencimento")
+		Optional<List<Asocontrole>> findAllFuncaoTipo(
+		@Param("idfuncao") int idfuncao,
+		@Param("nome") String nome,
+		@Param("idasotipo") int idtipo);
+		
+		//Funcao e Loja
+		@Query("select a from Asocontrole a where a.funcionario.funcao.idfuncao= :idfuncao " 
+				+ " and a.finalizado=0 and a.funcionario.loja.idloja= :idloja and a.funcionario.nome like CONCAT('%', :nome, '%') order by a.datavencimento")
+		Optional<List<Asocontrole>> findAllFuncaoLoja(
+		@Param("idfuncao") int idfuncao,
+		@Param("nome") String nome,
+		@Param("idloja") int idloja);
+		
+		//Loja e Tipo
+		@Query("select a from Asocontrole a where a.funcionario.loja.idloja= :idloja " 
+				+ " and a.finalizado=0 and a.asotipo.idasotipo= :idasotipo and a.funcionario.nome like CONCAT('%', :nome, '%') order by a.datavencimento")
+		Optional<List<Asocontrole>> findAllLojaTipo(
+		@Param("idasotipo") int idasotipo,
+		@Param("nome") String nome,
+		@Param("idloja") int idloja);		
+	
+		
+		//Somnte Loja
+		@Query("select a from Asocontrole a where a.funcionario.loja.idloja= :idloja " 
+				+ " and a.finalizado=0 and a.funcionario.nome like CONCAT('%', :nome, '%') order by a.datavencimento")
+		Optional<List<Asocontrole>> findAllLoja(
+		@Param("idloja") int idloja,
+		@Param("nome") String nome);		
+		
+		//Somnte fucao
+		@Query("select a from Asocontrole a where a.funcionario.funcao.idfuncao= :idfuncao " 
+				+ " and a.finalizado=0 and a.funcionario.nome like CONCAT('%', :nome, '%') order by a.datavencimento")
+		Optional<List<Asocontrole>> findAllFuncao(
+		@Param("idfuncao") int idfuncao,
+		@Param("nome") String nome);	
+		
+		//Somnte Tipo
+		@Query("select a from Asocontrole a where a.asotipo.idasotipo= :idasotipo " 
+				+ " and a.finalizado=0 and a.funcionario.nome like CONCAT('%', :nome, '%') order by a.datavencimento")
+		Optional<List<Asocontrole>> findAllTipo(
+		@Param("idasotipo") int idasotipo,
+		@Param("nome") String nome);	
+		
+		@Query("select a from Asocontrole a where a.finalizado=0 and a.funcionario.nome like CONCAT('%', :nome, '%') order by a.datavencimento")
+		Optional<List<Asocontrole>> findAllNome(
+		@Param("nome") String nome);	
+		
+		
+		//todos os parametros
+		@Query("select a from Asocontrole a where a.datavencimento>= :dataVencimentoStar and a.datavencimento<= :dataVencimentoEnd "
+				+ " and a.finalizado=0 and a.funcionario.loja.idloja= :idloja and "
+				+ "a.funcionario.funcao.idfuncao= :idfuncao  and "
+				+ "a.asotipo.idasotipo= :idtipo and a.funcionario.nome like CONCAT('%', :nome, '%') order by a.datavencimento")
+		Optional<List<Asocontrole>> findAll(
+		@Param("dataVencimentoStar") Date dataVencimentoStart,
+		@Param("dataVencimentoEnd") Date dataVencimentoEnd,
+		@Param("nome") String nome,
+		@Param("idloja") int idloja,
+		@Param("idfuncao") int idfuncao,
+		@Param("idtipo") int idtipo);
+	
+
 	
 }
