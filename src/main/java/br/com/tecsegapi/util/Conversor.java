bd73452b5d51684444c7393a0e0ac77e71484946
julioizidoro <jizidoro@globo.com>
2020-05-12 15:40:40 -0300

    calculo de dias

--- src/main/java/br/com/tecsegapi/util/Conversor.java
+++ src/main/java/br/com/tecsegapi/util/Conversor.java
@@ -88,6 +88,14 @@ public class Conversor {
 		}else mesAno = String.valueOf(mes) + "/" + String.valueOf(ano);
 		return mesAno;
 	}
+	
+	public int getRestoMes(int mes) {
+		if ((mes==1) || (mes==3) || (mes==5) || (mes==7) || (mes==8) || (mes==10) || (mes==12)) {
+			return 31;
+		} else if ((mes==4) || (mes==6) || (mes==9) || (mes==11)) {
+			return 30;
+		} else return 28;
+	}
 
 }
 
